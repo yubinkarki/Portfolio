1937c6aec2fd86e198fe40f090451dda02ad9c74
yubinkarki <iamyubinkarki@gmail.com>
2024-03-13 19:53:57 +0545

    Build files for release-v18

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "v1";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch and return from cache if available, else fetch from network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
